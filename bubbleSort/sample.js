0a3bb17172a3cc6049747ace26ba8b4b0d405276
shawn <shawn@salin.co.kr>
2021-01-06 10:22:57 +0900

    added sample code for bubble

---
+++ bubbleSort/sample.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+  let temp;
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // swap
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+      console.log(arr, arr[j], arr[j + 1], j);
+    }
+    console.log("one looping completed");
+  }
+
+  console.log("sorted arr ::", arr);
+}
+
+bubbleSort([8, -3, 29, 12, 38, 70, 45]);
