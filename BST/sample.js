bfc0632a18af7605050e98c5c726c4097e8bfbec
shawnscoding <shawnscoding@gmail.com>
2021-01-28 21:42:04 +0900

    implemented BST

---
+++ BST/sample.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (current.val === newNode.val) {
+        return;
+      }
+      if (newNode.val > current.val) {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      } else {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      }
+    }
+  }
+}
+
+let tree = new BinarySearchTree();
