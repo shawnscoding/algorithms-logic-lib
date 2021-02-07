0e5dd85a858fc63eadf58d713fba3fc16aa0c017
shawnscoding <shawnscoding@gmail.com>
2021-02-07 18:45:17 +0900

    implemented dynamic programming with fibonacci

---
+++ dynamicProgramming/fibonacci.js
@@ -0,0 +1,50 @@
+// time complexity :: O(2^n)
+// naive approach
+// const fib = (n) => {
+//   let result;
+//   if (n === 1 || n === 2) {
+//     result = 1;
+//   } else {
+//     result = fib(n - 1) + fib(n - 2);
+//   }
+//   return result;
+// };
+
+// console.log(fib(50));
+
+// time complexity :: O(n)
+// memoized solution
+// const memo = [];
+// const fib = (n, _memo) => {
+//   let result;
+
+//   if (_memo[n]) {
+//     return _memo[n];
+//   }
+//   if (n === 1 || n === 2) {
+//     result = 1;
+//   } else {
+//     result = fib(n - 1, _memo) + fib(n - 2, _memo);
+//     _memo[n] = result;
+//   }
+//   return result;
+// };
+
+// console.log(fib(50, memo));
+
+// bottom-up approach
+// time complexity :: O(n)
+const fibBottomUp = (n) => {
+  const memoArr = [];
+  memoArr[1] = 1;
+  memoArr[2] = 1;
+  for (let i = 3; i <= n; i++) {
+    memoArr[i] = memoArr[i - 1] + memoArr[i - 2];
+    // 2
+    if (i === n) {
+      return memoArr[i];
+    }
+  }
+  return memoArr[1];
+};
+console.log(fibBottomUp(50));
--- weightedGraph/shortestWay.js
+++ weightedGraph/shortestWay.js
@@ -1,16 +1,72 @@
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
 class PriorityQueue {
   constructor() {
-    this.values = [];
+    this.queue = [];
   }
+
   enqueue(val, priority) {
-    this.values.push({ val, priority });
-    this.sort();
+    let newNode = new Node(val, priority);
+    this.queue.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.queue.length - 1;
+    const element = this.queue[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.queue[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.queue[parentIdx] = element;
+      this.queue[idx] = parent;
+      idx = parentIdx;
+    }
   }
   dequeue() {
-    return this.values.shift();
+    const min = this.queue[0];
+    const end = this.queue.pop();
+    if (this.queue.length > 0) {
+      this.queue[0] = end;
+      this.sinkDown();
+    }
+    return min;
   }
-  sort() {
-    this.values.sort((a, b) => a.priority - b.priority);
+  sinkDown() {
+    let idx = 0;
+    const length = this.queue.length;
+    const element = this.queue[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.queue[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.queue[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.queue[idx] = this.queue[swap];
+      this.queue[swap] = element;
+      idx = swap;
+    }
   }
 }
 
@@ -44,7 +100,7 @@ class WeightedGraph {
     }
     // console.log("nodes ::", nodes);
     // as long as there is something to visit
-    while (nodes.values.length) {
+    while (nodes.queue.length) {
       smallest = nodes.dequeue().val;
       if (smallest === finish) {
         //WE ARE DONE
