979d50cc3235eba8c0d60d8644e863f0549af77e
shawnscoding <shawnscoding@gmail.com>
2021-02-14 13:59:32 +0900

    did coding challenges

--- encoding/romanNumerals.js
+++ encoding/romanNumerals.js
@@ -8,4 +8,22 @@
 // Input: "XLVI"
 // Output: 46
 // Tags
-// string manipulation searching encoding
+// string manipulationsearchingencoding
+function BasicRomanNumerals(str) {
+  var roman = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  var number = 0;
+
+  for (let i = 0; i <= str.length - 1; i++) {
+    if (roman[str[i + 1]] > roman[str[i]]) {
+      number += roman[str[i + 1]] - roman[str[i]];
+      i += 1;
+    } else {
+      number += roman[str[i]];
+    }
+  }
+
+  return number;
+}
+
+// keep this function call here
+console.log(BasicRomanNumerals(readline()));
